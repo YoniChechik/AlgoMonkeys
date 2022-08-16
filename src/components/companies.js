8950227e9a82e1bbab7272e9b44f209a46b287df
Yoni Chechik <chechik.yoni@gmail.com>
2022-08-16 18:44:19 +0300

    fix more svg

--- src/components/companies.js
+++ src/components/companies.js
@@ -10,18 +10,18 @@ export default function Companies() {
         Happy <PinkPart>Costumers</PinkPart>
       </Title>
       <div className={styles.flex_list}>
-        <Image className={styles.img} img={require("/img/logos/buildots_black.svg")} alt="Buildots logo" />
-        <Image className={styles.img} img={require("/img/logos/deepchecks_black.svg")} alt="DeepChecks logo" />
-        <Image className={styles.img} img={require("/img/logos/classiq_black.svg")} alt="Classiq logo" />
+        <Image className={styles.img} img={"/img/logos/buildots_black.svg"} alt="Buildots logo" />
+        <Image className={styles.img} img={"/img/logos/deepchecks_black.svg"} alt="DeepChecks logo" />
+        <Image className={styles.img} img={"/img/logos/classiq_black.svg"} alt="Classiq logo" />
         <Image className={styles.img} img={require("/img/logos/applydesign_black.png")} alt="ApplyDesign logo" />
-        <Image className={styles.img} img={require("/img/logos/carevature_black.svg")} alt="Carevature logo" />
-        <Image className={styles.img} img={require("/img/logos/clarityo.svg")} alt="ClarityO logo" />
-        <Image className={styles.img} img={require("/img/logos/ares.svg")} alt="Ares logo" />
+        <Image className={styles.img} img={"/img/logos/carevature_black.svg"} alt="Carevature logo" />
+        <Image className={styles.img} img={"/img/logos/clarityo.svg"} alt="ClarityO logo" />
+        <Image className={styles.img} img={"/img/logos/ares.svg"} alt="Ares logo" />
         <Image className={styles.img} img={require("/img/logos/ador.png")} alt="AdOr logo" />
-        <Image className={styles.img} img={require("/img/logos/eyecontrol_black.svg")} alt="EyeControl logo" />
+        <Image className={styles.img} img={"/img/logos/eyecontrol_black.svg"} alt="EyeControl logo" />
         <Image className={styles.img} img={require("/img/logos/firmus_black.png")} alt="Firmus logo" />
-        <Image className={styles.img} img={require("/img/logos/mdhair.svg")} alt="MDHair logo" />
-        <Image className={styles.img} img={require("/img/logos/itc.svg")} alt="ITC logo" />
+        <Image className={styles.img} img={"/img/logos/mdhair.svg"} alt="MDHair logo" />
+        <Image className={styles.img} img={"/img/logos/itc.svg"} alt="ITC logo" />
         <Image className={styles.img} img={require("/img/logos/medhub_black.png")} alt="MedHub logo" />
       </div>
     </section>

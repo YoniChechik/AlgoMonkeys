73dba68893c8c7d6e6228a07288aab1734a08f65
Yoni Chechik <chechik.yoni@gmail.com>
2022-08-16 18:37:48 +0300

    update to svg

--- src/components/what_we_do.js
+++ src/components/what_we_do.js
@@ -1,13 +1,12 @@
 import React from "react";
 import styles from "./what_we_do.module.css";
 import Title, { PinkPart } from "./title";
-import Image from "@theme/IdealImage";
 
-const DoBlock = ({ title, image_path, alt, text_div }) => {
+const DoBlock = ({ title, svg_path, alt, text_div }) => {
   return (
     <div className={styles.flex_block}>
       <div className={styles.title_and_icon}>
-        <Image className={styles.icon} img={require(image_path)} alt={alt} />
+        <img className={styles.icon} img={svg_path} alt={alt} />
         <h2>{title}</h2>
       </div>
       {text_div}
@@ -94,22 +93,17 @@ export default function WhatWeDo() {
         What We <PinkPart>Do Best</PinkPart>
       </Title>
       <div className={styles.flex_list}>
-        <DoBlock
-          title="Computer Vision"
-          image_path="/img/what_we_do_icons/eye.svg"
-          alt="eye icon"
-          text_div={cv_div()}
-        />
-        <DoBlock title="3D & SLAM" image_path="/img/what_we_do_icons/ar.svg" alt="3D icon" text_div={slam_div()} />
+        <DoBlock title="Computer Vision" svg_path="/img/what_we_do_icons/eye.svg" alt="eye icon" text_div={cv_div()} />
+        <DoBlock title="3D & SLAM" svg_path="/img/what_we_do_icons/ar.svg" alt="3D icon" text_div={slam_div()} />
         <DoBlock
           title="Data Science"
-          image_path="/img/what_we_do_icons/data.svg"
+          svg_path="/img/what_we_do_icons/data.svg"
           alt="data science icon"
           text_div={ds_div()}
         />
         <DoBlock
           title="Idea to Production"
-          image_path="/img/what_we_do_icons/cloud_chip.svg"
+          svg_path="/img/what_we_do_icons/cloud_chip.svg"
           alt="cloud and chip icon"
           text_div={prod_div()}
         />

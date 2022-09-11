3d1f7f6e88ad14757bb496bcbf5978b5b2688b69
Yoni Chechik <chechik.yoni@gmail.com>
2022-09-11 18:59:39 +0300

    araia label to links

--- src/components/companies.js
+++ src/components/companies.js
@@ -21,44 +21,48 @@ export default function Companies() {
         Happy <PinkPart>Customers</PinkPart>
       </Title>
       <div className={styles.flex_list}>
-        <Link className={styles.img} to="https://buildots.com/">
+        <Link className={styles.img} aria-label="Buildots website" to="https://buildots.com/">
           <BuildotsLogo title="Buildots logo" aria-label="Buildots logo" role="img" />
         </Link>
-        <Link className={styles.img} to="https://deepchecks.com/">
+        <Link className={styles.img} aria-label="DeepChecks website" to="https://deepchecks.com/">
           <DeepchecksLogo title="DeepChecks logo" aria-label="DeepChecks logo" role="img" />
         </Link>
-        <Link className={styles.img} to="https://www.classiq.io/">
+        <Link className={styles.img} aria-label="Classiq website" to="https://www.classiq.io/">
           <ClassiqLogo title="Classiq logo" aria-label="Classiq logo" role="img" />
         </Link>
-        <Link className={styles.img} to="https://www.applydesign.io/">
+        <Link className={styles.img} aria-label="xxx website" to="https://www.applydesign.io/">
           <Image img={require("/img/logos/applydesign_black.png")} alt="ApplyDesign logo" />
         </Link>
-        <Link className={styles.img} to="https://www.carevature.com/">
+        <Link className={styles.img} aria-label="ApplyDesign website" to="https://www.carevature.com/">
           <CarevatureLogo title="Carevature logo" aria-label="Carevature logo" role="img" />
         </Link>
-        <Link className={styles.img} to="https://clarityo.com/">
+        <Link className={styles.img} aria-label="Clarityo website" to="https://clarityo.com/">
           <ClarityoLogo title="Clarityo logo" aria-label="Clarityo logo" role="img" />
         </Link>
-        <Link className={styles.img} to="https://www.linkedin.com/company/connected-fitness-startup-stealth/">
+        <Link
+          className={styles.img}
+          aria-label="Ares website"
+          to="https://www.linkedin.com/company/connected-fitness-startup-stealth/"
+        >
           <AresLogo title="Ares logo" aria-label="Ares logo" role="img" />
         </Link>
         <Image img={require("/img/logos/ador.png")} className={styles.img} alt="AdOr logo" />
-        <Link className={styles.img} to="https://www.eyecontrol.co.il/">
+        <Link className={styles.img} aria-label="EyeControl website" to="https://www.eyecontrol.co.il/">
           <EyeControlLogo title="EyeControl logo" aria-label="EyeControl logo" role="img" />
         </Link>
-        <Link className={styles.img} to="https://www.firmus.ai/">
+        <Link className={styles.img} aria-label="Firmus website" to="https://www.firmus.ai/">
           <Image img={require("/img/logos/firmus_black.png")} alt="Firmus logo" />
         </Link>
-        <Link className={styles.img} to="https://www.mdhair.co/">
+        <Link className={styles.img} aria-label="MDHair website" to="https://www.mdhair.co/">
           <MDHairLogo title="MDHair logo" aria-label="MDHair logo" role="img" />
         </Link>
-        <Link className={styles.img} to="https://www.itc.tech/">
+        <Link className={styles.img} aria-label="ITC website" to="https://www.itc.tech/">
           <ITCLogo title="ITC logo" aria-label="ITC logo" role="img" />
         </Link>
-        <Link className={styles.img} to="https://www.medhub-ai.com/">
+        <Link className={styles.img} aria-label="MedHub website" to="https://www.medhub-ai.com/">
           <Image img={require("/img/logos/medhub_black.png")} alt="MedHub logo" />
         </Link>
-        <Link className={styles.img} to="https://www.simplex-mapping.com/">
+        <Link className={styles.img} aria-label="Simplex website" to="https://www.simplex-mapping.com/">
           <SimplexLogo title="Simplex logo" aria-label="Simplex logo" role="img" />
         </Link>
       </div>
